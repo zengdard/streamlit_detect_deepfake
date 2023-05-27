cac7147714fe335db5153f45f76c16444bf36687
Nielzaclord <61885397+Nielzaclord@users.noreply.github.com>
2023-05-27 16:36:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import os
 import requests
 import keras
-from keras_nlp.tokenizers import UnicodeCodepointTokenizer
 from keras.models import load_model
 import streamlit as st
 from PIL import Image
 import numpy as np
 
 from PIL import Image, ImageChops, ImageEnhance
-import os
-import itertools
 
 os.environ["HUGGINGFACE_TOKEN"] = "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"
 
@@ -27,8 +24,6 @@ def load_keras_model_from_hub(model_id):
     local_path = "model_casia_run1.h5"
     download_file(model_url, local_path)
 
-    
-    
 def prepare_image(image_path):
     return np.array(convert_to_ela_image(image_path, 90).resize(image_size)).flatten() / 255.0
 
@@ -37,7 +32,7 @@ def convert_to_ela_image(path, quality):
     ela_filename = 'temp_ela.png'
     
     image = Image.open(path).convert('RGB')
-    image.save(temp_filename, 'JPEG', quality = quality)
+    image.save(temp_filename, 'JPEG', quality=quality)
     temp_image = Image.open(temp_filename)
     
     ela_image = ImageChops.difference(image, temp_image)
@@ -50,44 +45,31 @@ def convert_to_ela_image(path, quality):
     
     ela_image = ImageEnhance.Brightness(ela_image).enhance(scale)
     
-    return ela_image 
+    return ela_image
 
 st.set_page_config(page_title="Fake Domain Detector", layout="wide")
 
 st.title("Fake Domain Detector (Beta) - StendhalGPT Security")
-# Préparer l'image
-
-
-# Fonction pour hachurer l'image
-def apply_hatching(image, percentage):
-    hatched_image = np.array(image)
-    h, w, _ = hatched_image.shape
-    mask = np.random.choice([0, 1], size=(h, w), p=[1 - percentage, percentage])
-    hatched_image[mask == 1] = [255, 0, 0]  # Couleur rouge pour le hachurage
-    return hatched_image
-
-# Interface Streamlit
-st.title("Détection de fausses images")
 
 # Charger l'image
 uploaded_file = st.file_uploader("Choisissez une image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    try: 
+    try:
         model = load_model('model_casia_run1.h5')
         model.compile(optimizer="adam", loss='categorical_crossentropy', metrics=['accuracy'])
 
     except:
         load_keras_model_from_hub('Nielzac/Altered_Picture_Model')
-        
+
     image = Image.open(uploaded_file)
     st.image(image, caption="Image originale", use_column_width=True)
     
     image.save("chemin_de_sauvegarde.jpg")
-    model = load_model('model_casia_run1.h5',compile=False)
+    model = load_model('model_casia_run1.h5', compile=False)
     model.compile(optimizer="adam", loss='categorical_crossentropy', metrics=['accuracy'])
-    image = prepare_image('chemin_de_sauvegarde.jpg')
-    X = np.array(image)
+    prepared_image = prepare_image('chemin_de_sauvegarde.jpg')
+    X = np.array(prepared_image)
     X = X.reshape(-1, 128, 128, 3)
     # Prédiction
     prediction = model.predict(X)
@@ -95,5 +77,5 @@ if uploaded_file is not None:
     st.write(f"Probabilité d'être fausse : {fake_percentage:.2f}%")
 
     # Appliquer le hachurage
-    hatched_image = apply_hatching(image, fake_percentage / 100)
+    hatched_image = apply_hatching(prepared_image, fake_percentage / 100)
     st.image(hatched_image, caption="Image avec hachurage", use_column_width=True)

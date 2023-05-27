50264ef5ec79c6c132dcdf8878f9d827ca1c8a54
Nielzaclord <61885397+Nielzaclord@users.noreply.github.com>
2023-05-27 17:20:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ def load_keras_model_from_hub(model_id):
     model_url = f"https://huggingface.co/Nielzac/Altered_Picture_Model/resolve/main/model_casia_run1.h5"
     local_path = "model_casia_run1.h5"
     download_file(model_url, local_path)
-
 def apply_hatching(image, percentage):
     # Convertir l'image en tableau NumPy
     image_array = np.array(image)
@@ -34,18 +33,20 @@ def apply_hatching(image, percentage):
     # Calculer le nombre de traits à dessiner
     num_lines = int(height * width * (percentage / 100))
 
-    # Générer des coordonnées aléatoires pour les traits
-    line_coords = np.random.randint(0, high=height, size=(num_lines, 2))
+    # Générer les coordonnées de début et de fin des traits diagonaux
+    line_coords = np.random.randint(0, high=width, size=(num_lines, 2))
+    line_coords[:, 1] = np.random.randint(0, high=height, size=num_lines)
+    line_coords[:, 2] = line_coords[:, 0] + np.random.randint(1, high=width - line_coords[:, 0], size=num_lines)
+    line_coords[:, 3] = line_coords[:, 1] + np.random.randint(1, high=height - line_coords[:, 1], size=num_lines)
 
-    # Dessiner les traits sur l'image
+    # Dessiner les traits diagonaux sur l'image
+    drawn_image = Image.fromarray(image_array)
+    draw = ImageDraw.Draw(drawn_image)
     for coord in line_coords:
-        x, y = coord
-        image_array[x, y, :] = [255, 0, 0]  # Couleur rouge pour les traits
+        x1, y1, x2, y2 = coord
+        draw.line((x1, y1, x2, y2), fill=(255, 0, 0), width=1)
 
-    # Convertir le tableau NumPy en image PIL
-    hatched_image = Image.fromarray(image_array)
-
-    return hatched_image
+    return drawn_image
 def prepare_image(image_path):
     return np.array(convert_to_ela_image(image_path, 90).resize(image_size)).flatten() / 255.0
 

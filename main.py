48f26a25a1de5a984f77fbe1e9fee5216743de2c
Nielzaclord <61885397+Nielzaclord@users.noreply.github.com>
2023-05-27 21:22:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -55,16 +55,18 @@ def apply_fake_filter(image, fake_score, add_text):
     # Specify the coordinates of the text in the middle of the image
     text_width, text_height = draw.textsize(text, font)
     text_x = (width - text_width) // 2
-    text_y = (height - text_height) // 2 + 50
+    
+ # Calculate the additional text's coordinates
+    additional_text_width, additional_text_height = draw.textsize(add_text, font)
+    additional_text_x = (width - additional_text_width) // 2
+    additional_text_y = height // 2 + 40
+    
+    text_y = height // 2 + additional_text_height + 30
 
     # Draw the text on the image in white
     draw.text((text_x, text_y), text, font=font, fill=(255, 255, 255))
     
     
- # Calculate the additional text's coordinates
-    additional_text_width, additional_text_height = draw.textsize(add_text, font)
-    additional_text_x = (width - additional_text_width) // 2
-    additional_text_y = text_y + text_height + 140
 
     # Draw the additional text on the image in white
     draw.text((additional_text_x, additional_text_y), additional_text, font=font, fill=(255, 255, 255))
